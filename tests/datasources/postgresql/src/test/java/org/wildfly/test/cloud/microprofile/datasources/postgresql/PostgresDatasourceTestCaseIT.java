c105847ad57446a9af5a005058309237e5cdedd4
Kabir Khan <kkhan@redhat.com>
2022-04-26 15:38:15 +0100

    Add postgres ds test using datasource provisioned in image

---
+++ tests/datasources/postgresql/src/test/java/org/wildfly/test/cloud/microprofile/datasources/postgresql/PostgresDatasourceTestCaseIT.java
@@ -0,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *  Copyright 2022 Red Hat, Inc., and individual contributors
+ *  as indicated by the @author tags.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.wildfly.test.cloud.microprofile.datasources.postgresql;
+
+import io.dekorate.testing.annotation.Inject;
+import io.fabric8.kubernetes.api.model.KubernetesList;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.wildfly.test.cloud.common.WildFlyCloudTestCase;
+import org.wildfly.test.cloud.common.WildFlyKubernetesIntegrationTest;
+
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+@WildFlyKubernetesIntegrationTest
+public class PostgresDatasourceTestCaseIT extends WildFlyCloudTestCase {
+
+    @Inject
+    private KubernetesClient client;
+
+    @Inject
+    private KubernetesList list;
+
+    @Test
+    public void checkMPConfig() throws Exception {
+        int status = getHelper().doWithWebPortForward("first",
+                url -> RestAssured.given().header("Content-Type", MediaType.TEXT_PLAIN).post(url).getStatusCode());
+        Assertions.assertEquals(200, status);
+        status = getHelper().doWithWebPortForward("second",
+                url -> RestAssured.given().header("Content-Type", MediaType.TEXT_PLAIN).post(url).getStatusCode());
+        Assertions.assertEquals(200, status);
+
+        List<String> list = new ArrayList<>();
+        list = getHelper().doWithWebPortForward("", url -> {
+            Response r = RestAssured.get(url);
+            Assertions.assertEquals(200, r.getStatusCode());
+            return r.as(List.class);
+        });
+
+        Assertions.assertArrayEquals(new String[]{"first", "second"}, list.toArray(new String[list.size()]));
+    }
+
+}
